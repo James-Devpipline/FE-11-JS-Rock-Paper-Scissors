1752ae413fcf0b6657a66e9b6dc48c61c6f6b4b2
James Hales <james.devpipeline.com>
2023-02-28 20:22:30 -0700

    added while loop in place

--- script.js
+++ script.js
@@ -13,61 +13,18 @@ Player score: 0
 Bot score: 0
 Draws: 0
 */
+function rpcGame() {
+  let gameInPlay = true
 
-const scores = {
-  Player : 0,
-  Bot : 0,
-  Draws : 0
-}
+  while (gameInPlay) {
+    const scores = {
+      Player : 0,
+      Bot : 0,
+      Draws : 0
+    }
 
-
-function gameRules(userWep, botWep) {
-  if (userWep == botWep) {
-    alert("Draw!")
-    scores.Draws ++
-  }
-
-  else if (userWep.toUpperCase() === 'R' && botWep.toUpperCase()  === 'S') {
-    alert("Rock beats Scissors! You win!")
-    scores.Player ++
-    // alert(`Player: ${scores.Player} Bot: ${scores.Bot} Draws: ${scores.Draws}`)
-  }
-  
-  else if (userWep.toUpperCase()  === 'S' && botWep.toUpperCase()  === 'R') {
-    alert("Rock beats Scissors! You lose!")
-    scores.Bot ++
-  }
-
-  else if (userWep.toUpperCase()  === 'S' && botWep.toUpperCase()  === 'P') {
-    alert("Scissors beat paper! You win!")
-    scores.Player ++
-  }
-
-  else if (userWep.toUpperCase()  === 'P' && botWep.toUpperCase()  === 'S') {
-    alert("Scissors beat paper! You lose!")
-    scores.Bot ++
-  }
-
-  else if (userWep.toUpperCase()  === 'P' && botWep.toUpperCase()  === 'R') {
-    alert("Paper beats Rock! You win!")
-    scores.Player ++
-  }
-  
-  else if (userWep.toUpperCase()  === 'R' && botWep.toUpperCase()  === 'P') {
-    alert("Paper beats Rock! You lose!")
-    scores.Bot ++
-  }
-}
-
-function botRandomNum() {
-  return Math.floor(Math.random() * (4 - 1) + 1)
-}
-
-console.log(botRandomNum())
-
-
-function weaponSelect(user, bot) {
-  if (user) {
+    
+      
     let rpcUserInput = prompt(`Player: ${scores.Player} Bot: ${scores.Bot} Draws: ${scores.Draws} [1] Rock \n[2] Paper \n[3] Scissor \n[Q]uit`)
     
     if (rpcUserInput === "1") {
@@ -85,6 +42,7 @@ function weaponSelect(user, bot) {
     else if (rpcUserInput.toUpperCase() == "Q") {
       alert("Exiting game")
       gameInPlay = !gameInPlay
+      alert(gameInPlay)
       console.log(gameInPlay)
     }
         
@@ -92,32 +50,91 @@ function weaponSelect(user, bot) {
       alert("INVALID INPUT")
       console.error("INVALID INPUT")
     }
-  }
   
-  if (bot) {
-    const botWeaponNum = Math.floor(Math.random() * (4 - 1) + 1)
-    console.log(botWeaponNum)
+  
     let botWeapon = null
-    if (botWeaponNum === 1){
-      botWeapon = "R"
+    function botWeaponSelect() {
+      const botWeaponNum = Math.floor(Math.random() * (4 - 1) + 1)
+      console.log(botWeaponNum)
+      if (botWeaponNum === 1){
+        botWeapon = "R"
+      }
+  
+      else if (botWeaponNum === 2) {
+        botWeapon = "P"
+      }
+  
+      else if (botWeaponNum === 3) {
+        botWeapon = "S"
+      }
+      alert(botWeapon)
+    }
+      
+      
+      
+    
+
+
+    function gameRules(userWep, botWep) {
+      if (userWep == botWep) {
+        alert("Draw!")
+        scores.Draws ++
+      }
+
+      else if (userWep.toUpperCase() === 'R' && botWep.toUpperCase()  === 'S') {
+        alert("Rock beats Scissors! You win!")
+        scores.Player ++
+        // alert(`Player: ${scores.Player} Bot: ${scores.Bot} Draws: ${scores.Draws}`)
+      }
+      
+      else if (userWep.toUpperCase()  === 'S' && botWep.toUpperCase()  === 'R') {
+        alert("Rock beats Scissors! You lose!")
+        scores.Bot ++
+      }
+
+      else if (userWep.toUpperCase()  === 'S' && botWep.toUpperCase()  === 'P') {
+        alert("Scissors beat paper! You win!")
+        scores.Player ++
+      }
+
+      else if (userWep.toUpperCase()  === 'P' && botWep.toUpperCase()  === 'S') {
+        alert("Scissors beat paper! You lose!")
+        scores.Bot ++
+      }
+
+      else if (userWep.toUpperCase()  === 'P' && botWep.toUpperCase()  === 'R') {
+        alert("Paper beats Rock! You win!")
+        scores.Player ++
+      }
+      
+      else if (userWep.toUpperCase()  === 'R' && botWep.toUpperCase()  === 'P') {
+        alert("Paper beats Rock! You lose!")
+        scores.Bot ++
+      }
     }
 
-    else if (botWeaponNum === 2) {
-      botWeapon = "P"
+    function botRandomNum() {
+      return Math.floor(Math.random() * (4 - 1) + 1)
     }
 
-    else if (botWeaponNum === 3) {
-      botWeapon = "S"
-    }
-    alert(botWeapon)
+    console.log(botRandomNum())
+
+
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
   }
-  
-  
 }
 
+rpcGame()
 
-
-weaponSelect(user = false, bot = true)
 
 
 // if (rpcUserInput === "1") {
